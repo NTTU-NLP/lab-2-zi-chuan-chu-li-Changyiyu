02986aa8f5000ac7eb7f23602f6c5330160ec642
Hong-Jie Dai <hongjiedai@gmail.com>
2017-02-19 23:48:09 +0800

    10311173 作業lab2

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(StreamReader sr = new StreamReader(@"D:\10311173\lab-2-zi-chuan-chu-li-Changyiyu\Data\Input.txt"))
+            {
+                while(sr.Peek() != -1)
+                {
+                    string line = sr.ReadLine();
+                    foreach(var ch in line)
+                    {     
+                        if (Char.IsPunctuation(ch))
+                        {
+                           if(ch == '.')
+                               Console.Write(ch + "\n");
+                           else if (ch == '?')
+                               Console.Write(ch + "\n");
+                           else if (ch == '!')
+                               Console.Write(ch + "\n");
+                           else
+                               Console.Write(ch);
+                        }
+                        else
+                            Console.Write(ch);
+                    }
+                }
+            }
+            Console.Read();
+        }
+    }
+}
